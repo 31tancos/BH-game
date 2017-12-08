92168b4219f1b21b2d6fdc010ff10caa2cc0b611
31CalTra <Sonicavenger@rogers.com>
2017-12-08 10:09:51 -0500

    Animated the studio logo

---
+++ fulcan game project/Assets/scipts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public string SceneToLoad = "";
+    public float Delay = 0;
+    void Start()
+    {
+        StartCoroutine(delayThenLoadScene());
+    }
+    IEnumerator delayThenLoadScene()
+    {
+        yield return new WaitForSeconds(Delay);
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
