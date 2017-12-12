8cce33249ba2b8e2181d5007328df7a9397797dc
31CalTra <Sonicavenger@rogers.com>
2017-12-12 12:18:57 -0500

    Tried making a code that changes the alpha

---
+++ fulcan game project/Assets/RandomAlphaEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomAlphaEffect : MonoBehaviour
+{
+    public Text Text;
+    public float Delay;
+    public float MinAlpha;
+    public float MaxAlpha;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Delay);
+            Text.material.color = new Color(
+                Text.color.r,
+                Text.color.g,
+                Text.color.b,
+                Random.Range(MinAlpha, MaxAlpha));
+            yield return null;
+        }
+    }
+}
