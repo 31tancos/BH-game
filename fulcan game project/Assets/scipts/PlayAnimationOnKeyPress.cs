33f1d679fe5fd343bb38c6a1e5a602d09409aa57
31CalTra <Sonicavenger@rogers.com>
2017-12-13 10:03:15 -0500

    Created an exit animation, started giving a key to trigger it

---
+++ fulcan game project/Assets/scipts/PlayAnimationOnKeyPress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimationOnKeyPress : MonoBehaviour {
+
+    // Use this for initialization
+    public KeyCode Key;
+    public string TriggerName;
+    public Animator Animator;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            Animator.SetTrigger(TriggerName);
+        }
+    }
+}
