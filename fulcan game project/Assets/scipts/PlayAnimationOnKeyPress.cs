e859d10bdcf7eba3681bc7813d0cedf6d4606c23
31CalTra <Sonicavenger@rogers.com>
2017-12-18 10:06:47 -0500

    Fixed the audio on key

--- fulcan game project/Assets/scipts/PlayAnimationOnKeyPress.cs
+++ fulcan game project/Assets/scipts/PlayAnimationOnKeyPress.cs
@@ -26,6 +26,7 @@ public class PlayAnimationOnKeyPress : MonoBehaviour {
         if ( acceptingInput && Input.GetKeyDown(Key))
         {
             Animator.SetTrigger(TriggerName);
+            acceptingInput = false;
         }
     }
 }
--- fulcan game project/Assets/scipts/PlayAudioOnKeyPress.cs
+++ fulcan game project/Assets/scipts/PlayAudioOnKeyPress.cs
@@ -6,11 +6,24 @@ public class PlayAudioOnKeyPress : MonoBehaviour {
 
     public KeyCode Key;
     public AudioSource TargetAudioSource;
+    public float InDelay = 1f;
+    private bool acceptingInput = false;
+    void Start()
+    {
+        StartCoroutine(waitThenAcceptInput());
+    }
+
+    IEnumerator waitThenAcceptInput()
+    {
+        yield return new WaitForSeconds(InDelay);
+        acceptingInput = true;
+    }
     void Update()
     {
-        if (Input.GetKeyDown(Key))
+        if (acceptingInput && Input.GetKeyDown(Key))
         {
             TargetAudioSource.Play();
+            acceptingInput = false;
         }
     }
 }
