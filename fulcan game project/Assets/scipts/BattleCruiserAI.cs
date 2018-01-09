c54971430739cc0a55b02d388ed908a92ca9076c
31CalTra <Sonicavenger@rogers.com>
2018-01-09 10:07:07 -0500

    Finished the move script for main character, experimented with physics

---
+++ fulcan game project/Assets/scipts/BattleCruiserAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCruiserAI : MonoBehaviour {
+
+    public float Health;
+    public float Force;
+    public Rigidbody2D RigidBody;
+
+    void Update()
+    {
+        if (Health != 0)
+        {
+            RigidBody.AddForce(Vector2.down * Force);
+            RigidBody.AddForce(Vector2.right * Force);
+        }
+    }
+}
--- fulcan game project/Assets/scipts/move.cs
+++ fulcan game project/Assets/scipts/move.cs
@@ -6,9 +6,28 @@ public class move : MonoBehaviour {
 
     public KeyCode UpKey;
     public KeyCode DownKey;
-    public KeyCode RightKey;
     public KeyCode LeftKey;
-    public RigidBody2D RigidBody;
-		
-	
+    public KeyCode RightKey;
+    public Rigidbody2D RigidBody;
+    public float Force;
+
+    void Update()
+    {
+        if (Input.GetKey(UpKey))
+        {
+            RigidBody.AddForce(Vector2.up * Force);
+        }
+        else if (Input.GetKey(DownKey))
+        {
+            RigidBody.AddForce(Vector2.down * Force);
+        }
+        if (Input.GetKey(LeftKey))
+        {
+            RigidBody.AddForce(Vector2.left * Force);
+        }
+        if (Input.GetKey(RightKey))
+        {
+            RigidBody.AddForce(Vector2.right * Force);
+        }
+    }
 }
