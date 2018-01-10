9f311c88fc38a743e34945465ec85a3c1bd64da1
31CalTra <Sonicavenger@rogers.com>
2018-01-10 10:07:54 -0500

    added in background, added in boundaries

--- fulcan game project/Assets/scipts/BattleCruiserAI.cs
+++ fulcan game project/Assets/scipts/BattleCruiserAI.cs
@@ -7,13 +7,26 @@ public class BattleCruiserAI : MonoBehaviour {
     public float Health;
     public float Force;
     public Rigidbody2D RigidBody;
+    public float MoveDelay = 1f;
 
-    void Update()
+    IEnumerator Start()
     {
+        RigidBody.AddForce(Vector2.right * Force / 2);
+        while (Health != 0)
+        {
+            RigidBody.AddForce(Vector2.left * Force);
+            yield return new WaitForSeconds(MoveDelay);
+            RigidBody.AddForce(Vector2.right * Force);
+            yield return new WaitForSeconds(MoveDelay);
+         }
+    }
+    /*void Update()
+    {
+
         if (Health != 0)
         {
             RigidBody.AddForce(Vector2.down * Force);
             RigidBody.AddForce(Vector2.right * Force);
         }
-    }
+    }*/
 }
